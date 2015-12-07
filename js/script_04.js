50bcc0d7a684c0f051944ed350a76b1d243ddcbf
Laura Chen <jhclaura@gmail.com>
2015-12-07 11:06:41 -0500

    update

--- js/script_04.js
+++ js/script_04.js
@@ -218,7 +218,7 @@ function render()
 
 		} else {
 			// if( videoo.readyState === videoo.HAVE_ENOUGH_DATA ) {
-				videoImageContext.drawImage(videoo, 0, 0, videoo.width, videoo.height);
+				videoImageContext.drawImage(videoo, 0, 0);
 				if(videoTexture)
 					videoTexture.needsUpdate = true;
 			// }
